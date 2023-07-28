1351f2cb941c8c8588518b98779cf0f91c445818
Fikri <fikrizhanking@gmail.com>
2023-07-28 15:10:28 +0700

    Add production mode

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import { Telegraf } from 'telegraf';
+import express from 'express';
 import * as dotenv from 'dotenv';
 dotenv.config();
 
@@ -11,8 +12,24 @@ const bot = new Telegraf(process.env.BOT_TOKEN);
 handler(bot);
 
 console.log('Telegram bot started');
-bot.launch();
 
-// Enable graceful stop
-process.once('SIGINT', () => bot.stop('SIGINT'));
-process.once('SIGTERM', () => bot.stop('SIGTERM'));
+// For production
+// use wabhook
+if(process.env.NODE_ENV == 'production') {
+	const app = express();
+	app.use(bot.webhookCallback('/' + process.env.SECRET_PATH));
+
+	// Mengatur webhook dengan URL yang sesuai
+	bot.telegram.setWebhook(process.env.WEBHOOK_URL + '/' + process.env.SECRET_PATH);
+
+	const port = process.env.PORT || 3000;
+	app.listen(port, () => {
+		console.log(`Server running at http://localhost:${port}`);
+	});
+} else {
+	bot.launch();
+
+	// Enable graceful stop
+	process.once('SIGINT', () => bot.stop('SIGINT'));
+	process.once('SIGTERM', () => bot.stop('SIGTERM'));
+}
